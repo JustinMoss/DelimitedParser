47b3e161f78ca8327619f99b3e6a240f967e49c1
Justin Moss <Justin.Moss@gmail.com>
2022-09-02 15:19:37 -0400

    Add additional docs

--- DelimitedParser.Domain/Person.cs
+++ DelimitedParser.Domain/Person.cs
@@ -1,4 +1,12 @@
 ﻿namespace DelimitedParser.Domain
 {
+    /// <summary>
+    /// Defines a Person record.
+    /// </summary>
+    /// <param name="LastName">The person's last name</param>
+    /// <param name="FirstName">The person's first name</param>
+    /// <param name="Email">The person's email address</param>
+    /// <param name="FavoriteColor">The person's favorite color</param>
+    /// <param name="DateOfBirth">The person's birth date</param>
     public record Person(string LastName, string FirstName, string Email, string FavoriteColor, DateTime DateOfBirth);
 }
--- DelimitedParser.Infrastructure/Utilities.cs
+++ DelimitedParser.Infrastructure/Utilities.cs
@@ -1,8 +1,16 @@
 ﻿namespace DelimitedParser.Infrastructure
 {
     // NOTE: There are so many ways to handle utilities like this. How would your team?
+    /// <summary>
+    /// Contains a set of cross class utility methods
+    /// </summary>
     public static class Utilities
     {
+        /// <summary>
+        /// Attempts to find an existent delimiter in an input string, returning space if none is found
+        /// </summary>
+        /// <param name="input">The searched input string</param>
+        /// <returns>A string delimiter</returns>
         public static string GetDelimiter(string input)
         {
             if (input is null)

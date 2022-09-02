006dc5d3071ba8c8544b2daf562759262758c32c
Justin Moss <Justin.Moss@ConstructConnect.com>
2022-09-02 00:11:35 -0400

    Add utility for determining delimiter

---
+++ DelimitedParser.Infrastructure/Utilities.cs
@@ -0,0 +1,15 @@
+﻿namespace DelimitedParser.Infrastructure
+{
+    // NOTE: There are so many ways to handle utilities like this. How would your team?
+    public static class Utilities
+    {
+        public static string GetDelimiter(string input)
+        {
+            if (input.Contains('|'))
+                return "|";
+            else if (input.Contains(','))
+                return ",";
+            return " ";
+        }
+    }
+}

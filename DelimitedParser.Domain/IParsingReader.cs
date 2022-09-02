108c1dd816b58ee24911e6ec58cf7f996d003c93
Justin Moss <Justin.Moss@ConstructConnect.com>
2022-09-02 00:14:55 -0400

    Add person parsing reader

---
+++ DelimitedParser.Domain/IParsingReader.cs
@@ -0,0 +1,25 @@
+﻿namespace DelimitedParser.Domain
+{
+    /// <summary>
+    /// Defines an object that parses files or text readers into an enumerable of objects of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of the objects being created</typeparam>
+    public interface IParsingReader<T>
+    {
+        /// <summary>
+        /// Parses a file into an enumerable of objects of type T, using the given delimiter.
+        /// </summary>
+        /// <param name="fileName">The name of the file to parse</param>
+        /// <param name="delimiter">The delimiter used for parsing</param>
+        /// <returns>An enumerable of objects of type T</returns>
+        IEnumerable<T> Parse(string fileName, string delimiter);
+
+        /// <summary>
+        /// Parses a text reader into an enumerable of objects of type T, using the given delimiter.
+        /// </summary>
+        /// <param name="reader">The name of the text reader to parse</param>
+        /// <param name="delimiter">The delimiter used for parsing</param>
+        /// <returns>An enumerable of objects of type T</returns>
+        IEnumerable<T> Parse(TextReader reader, string delimiter);
+    }
+}
---
+++ DelimitedParser.Infrastructure/PersonParsingReader.cs
@@ -0,0 +1,54 @@
+﻿using DelimitedParser.Domain;
+
+namespace DelimitedParser.Infrastructure
+{
+    /// <summary>
+    /// Defines an object that parses files or text readers into an enumerable of Person object.
+    /// </summary>
+    public class PersonParsingReader : IParsingReader<Person>
+    {
+        private readonly IParser<Person> _parser;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonParsingReader"/> class.
+        /// </summary>
+        /// <param name="parser">The parser used to create a person from a row</param>
+        public PersonParsingReader(IParser<Person> parser)
+        {
+            _parser = parser;
+        }
+
+        /// <summary>
+        /// Parses a file into an enumerable of Person objects, using the given delimiter.
+        /// </summary>
+        /// <param name="fileName">The name of the file to parse</param>
+        /// <param name="delimiter">The delimiter used for parsing</param>
+        /// <returns>An enumerable of Person objects</returns>
+        public IEnumerable<Person> Parse(string fileName, string delimiter) =>
+            Parse(new StreamReader(fileName), delimiter);
+
+        /// <summary>
+        /// Parses a text reader into an enumerable Person objects, using the given delimiter.
+        /// </summary>
+        /// <param name="reader">The name of the text reader to parse</param>
+        /// <param name="delimiter">The delimiter used for parsing</param>
+        /// <returns>An enumerable of Person objects</returns>
+        public IEnumerable<Person> Parse(TextReader reader, string delimiter)
+        {
+            var persons = new List<Person>();
+
+            var row = reader.ReadLine();
+            while (row != null)
+            {
+                delimiter ??= Utilities.GetDelimiter(row);
+
+                var person = _parser.Parse(row, delimiter);
+                persons.Add(person);
+
+                row = reader.ReadLine();
+            }
+
+            return persons;
+        }
+    }
+}

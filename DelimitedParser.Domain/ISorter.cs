c64842016055334b74995bf8753a1f52fd297693
Justin Moss <Justin.Moss@ConstructConnect.com>
2022-09-02 20:10:02 -0400

    Add a Person sorter

---
+++ DelimitedParser.Domain/ISorter.cs
@@ -0,0 +1,17 @@
+﻿namespace DelimitedParser.Domain
+{
+    /// <summary>
+    /// Defines an object that sorts enumerables of objects of type T
+    /// </summary>
+    /// <typeparam name="T">The type of the objects to sort</typeparam>
+    public interface ISorter<T>
+    {
+        /// <summary>
+        /// Gets a enumerable of objects of type T sorted by a given field name.
+        /// </summary>
+        /// <param name="items">The enumerable to sort</param>
+        /// <param name="fieldName">The field name for sorting</param>
+        /// <returns></returns>
+        IEnumerable<T> Sort(IEnumerable<T> items, string fieldName);
+    }
+}
---
+++ DelimitedParser.Infrastructure/PersonSorter.cs
@@ -0,0 +1,34 @@
+﻿using DelimitedParser.Domain;
+
+namespace DelimitedParser.Infrastructure
+{
+    /// <summary>
+    /// Defines an object that sorts enumerables of Person objects
+    /// </summary>
+    public class PersonSorter : ISorter<Person>
+    {
+        /// <summary>
+        /// Gets a enumerable Person objects sorted by a given field name.
+        /// </summary>
+        /// <param name="items">The enumerable to sort</param>
+        /// <param name="fieldName">The field name for sorting</param>
+        /// <returns>A sorted enumerable of Person objects</returns>
+        public IEnumerable<Person> Sort(IEnumerable<Person> items, string fieldName)
+        {
+            switch (fieldName)
+            {
+                case nameof(Person.LastName):
+                    return items.OrderByDescending(item => item.LastName);
+
+                case nameof(Person.DateOfBirth):
+                    return items.OrderBy(item => item.DateOfBirth);
+
+                case nameof(Person.FavoriteColor):
+                    return items.OrderBy(item => item.FavoriteColor).ThenByDescending(item => item.LastName);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(fieldName), fieldName + " is not a valid fieldName value.");
+            }
+        }
+    }
+}

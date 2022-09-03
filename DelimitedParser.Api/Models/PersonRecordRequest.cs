3bc0dae983c13ac2490236c1f883925b6c251636
Justin Moss <Justin.Moss@ConstructConnect.com>
2022-09-03 17:12:34 -0400

    Build out the rest api

---
+++ DelimitedParser.Api/Models/PersonRecordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DelimitedParser.Api.Models
+{
+    /// <summary>
+    /// Defines an object that represents the http request to add Person records.
+    /// </summary>
+    /// <param name="Record">The data line for parsing</param>
+    /// <param name="Delimiter">The delimiter to use when parsing</param>
+    public record PersonRecordRequest (string Record, string Delimiter);
+}
--- DelimitedParser.Api/Program.cs
+++ DelimitedParser.Api/Program.cs
@@ -1,9 +1,71 @@
+using DelimitedParser.Api.Models;
+using DelimitedParser.Domain;
+using DelimitedParser.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSingleton<IParser<Person>, PersonParser>();
+builder.Services.AddSingleton<IParsingReader<Person>, PersonParsingReader>();
+builder.Services.AddSingleton<IRepository<Person>, PersonRepository>();
+builder.Services.AddSingleton<ISorter<Person>, PersonSorter>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/records/{sortField}", GetSortedPersonListResponse);
+app.MapPost("/records", PostPersonRecords);
 
 app.Run();
+
+// NOTE: These could be moved off to another file, but given the overall size of
+// this application, it feel like overkill.
+IResult GetSortedPersonListResponse(string sortField, IRepository<Person> repository, ISorter<Person> sorter)
+{
+    try
+    {
+        string? fieldName =
+           sortField switch
+           {
+               "color" => nameof(Person.FavoriteColor),
+               "birthdate" => nameof(Person.DateOfBirth),
+               "name" => nameof(Person.LastName),
+               _ => null
+           };
+
+        if (fieldName is null)
+            return Results.Problem(sortField + " is not a valid search parameter.", statusCode: StatusCodes.Status400BadRequest);
+
+        var people = repository.GetAll();
+        if (!people.Any())
+            return Results.Problem("There were no people in the list to sort. Please try adding a person first.", statusCode: StatusCodes.Status500InternalServerError);
+
+        var sortedPeople = sorter.Sort(people, fieldName);
+
+        return Results.Ok(sortedPeople);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+    }
+}
+
+IResult PostPersonRecords(PersonRecordRequest request, IRepository<Person> repository, IParsingReader<Person> reader)
+{
+    try
+    {
+        if (string.IsNullOrEmpty(request.Record))
+            return Results.Problem("Required field 'Record' is missing", statusCode: StatusCodes.Status400BadRequest);
+
+        var people = reader.Parse(new StringReader(request.Record), request.Delimiter);
+
+        // NOTE: The requirements mention 'a single data line', which I interpret as a single string,
+        // which in theory could allow more than one record. Plus, it makes it easier to bulk add data
+        // for testing.
+        repository.AddBatch(people);
+
+        return Results.Ok("Successfully posted.");
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message, statusCode: StatusCodes.Status500InternalServerError);
+    }
+}

bcde52ecc3f81cc0d8095caafaa6106b86c0798a
Justin Moss <Justin.Moss@ConstructConnect.com>
2022-09-02 21:08:15 -0400

    Personal preference formatting fix, debatable

--- DelimitedParser.Infrastructure/PersonSorter.cs
+++ DelimitedParser.Infrastructure/PersonSorter.cs
@@ -13,22 +13,15 @@ namespace DelimitedParser.Infrastructure
         /// <param name="items">The enumerable to sort</param>
         /// <param name="fieldName">The field name for sorting</param>
         /// <returns>A sorted enumerable of Person objects</returns>
-        public IEnumerable<Person> Sort(IEnumerable<Person> items, string fieldName)
-        {
-            switch (fieldName)
+        public IEnumerable<Person> Sort(IEnumerable<Person> items, string fieldName) =>
+            // NOTE: This is a good place to discuss readability. Some may want a standard switch
+            // and full body method to make it more readable. I prefer compression, but flexible to the team.
+            fieldName switch
             {
-                case nameof(Person.LastName):
-                    return items.OrderByDescending(item => item.LastName);
-
-                case nameof(Person.DateOfBirth):
-                    return items.OrderBy(item => item.DateOfBirth);
-
-                case nameof(Person.FavoriteColor):
-                    return items.OrderBy(item => item.FavoriteColor).ThenByDescending(item => item.LastName);
-
-                default:
-                    throw new ArgumentOutOfRangeException(fieldName + " is not a valid fieldName value.", innerException: null);
-            }
-        }
+                nameof(Person.LastName) => items.OrderByDescending(item => item.LastName),
+                nameof(Person.DateOfBirth) => items.OrderBy(item => item.DateOfBirth),
+                nameof(Person.FavoriteColor) => items.OrderBy(item => item.FavoriteColor).ThenByDescending(item => item.LastName),
+                _ => throw new ArgumentOutOfRangeException(fieldName + " is not a valid fieldName value.", innerException: null),
+            };
     }
 }
